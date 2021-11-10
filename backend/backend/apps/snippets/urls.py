ef135347d4205cd5fceb36f271c98d361750fd98
zzsealy <1147575371@qq.com>
2021-11-10 13:36:30 +0000

    FIX: urls 文件配置错误

--- backend/backend/apps/snippets/urls.py
+++ backend/backend/apps/snippets/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight)
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
