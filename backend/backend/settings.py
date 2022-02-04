eec8bf3f91665e317477a8a068dbdaebb718a56f
zzsealy <1147575371@qq.com>
2022-02-04 21:11:37 +0800

    WIP: 添加docker 部署文件

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from backend.localsetting import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ DEBUG = True
 
 TOKEN_AGE = 30
 
-# ALLOWED_HOSTS = ['testserver', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,12 +115,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
