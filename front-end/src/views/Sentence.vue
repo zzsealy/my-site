2623a471a0ae1319f5c7636afe95ce137da5f715
zzsealy <1147575371@qq.com>
2022-07-26 15:32:37 +0000

    feat: 句子 前端 一部分

---
+++ front-end/src/views/Sentence.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="docu-body">
+        <el-row :gutter="20">
+            <el-col :span="14" :offset="4" style="padding: 0px">
+            </el-col>
+            <!-- document.getElementById('post-block').childNodes.length  这个是 统计元素子元素的数量-->
+            <el-col :span="4" style="padding: 0px">
+                <div class="divi-line" style="float: left; width: 1px; height: 1000px; background: #c8c8c8"></div>
+                <sider :cates="cates" :showPostNav=false :activeName="activeName"></sider>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+
+<script>
+
+
+export default({
+    name: 'Sentence',
+    data() {
+            return {
+        }
+    },
+    methods: {
+        
+    },
+
+})
+</script>
+
+
+<style>
+
+</style>
---
+++ front-end/src/views/admin/NewSentence.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <b-form-input v-model="sentenceBody" type="text" placeholder="句子"></b-form-input><br>
+        <b-form-input v-model="author" type="text" placeholder="作者"></b-form-input><br>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddSentence',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        addSentence(body, author, cate){
+            let path = this.$store.state.URL + '/sentence';
+            this.$axios.post(path , {'body': body, 'author': author, 'cate': cate})
+                .then((res) => {
+                    data = res.data;
+                    if(res.data.status_code == 200){
+                        this.$toasted.success('句子发表成功');
+                    }
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
