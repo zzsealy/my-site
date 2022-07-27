0a3d6ac3233f08e95c36a6ffa34a7f2ddfd8bb0d
zzsealy <1147575371@qq.com>
2022-07-27 20:23:43 +0800

    feat: 后台管理页面 改成侧边导航

--- front-end/src/components/Sider.vue
+++ front-end/src/components/Sider.vue
@@ -5,19 +5,15 @@
             <el-input class="search-input" type="text" name="" id="" placeholder="Search"> </el-input>
             <!-- :prefix-icon="el-icon-search" -->
         </div>
-        <div class="sider-cate-text">
-            <el-collapse v-model="activeNames" @change="handleChange">
-                <el-collapse-item title="分类" name="1">
-                    <div v-for="cate in cates" :key="cate.id">
-                        {{cate.name}}
-                    </div>
-                </el-collapse-item>
-                <el-collapse-item v-if="showPostNav" title="文章导航" name="2" @change="handleChange">
-                    <div v-for="nav in postNavList" :key="nav.id">
-                        <a class="post-nav-a" v-bind:href= "'#' + nav.id ">{{ nav.title }}</a>
-                    </div>
-                </el-collapse-item>
-            </el-collapse>
+        <div class="sider-cate-div">
+            <p class="cate-title"><i class="el-icon-document-copy"></i> 文章分类</p>
+            <el-divider class="divider"></el-divider>
+            <div @click="cateClick(cate.name)" v-for="cate in cates" :key="cate.id" class="cate-name">
+                <strong>{{cate.name}}</strong>
+            </div>
+        </div>
+        <div v-for="nav in postNavList" :key="nav.id">
+            <a class="post-nav-a" v-bind:href="'#' + nav.id ">{{ nav.title }}</a>
         </div>
 
     </div>
@@ -37,6 +33,11 @@
         methods: {
             handleChange(val) {
                 window.console.log(val);
+            },
+            cateClick(cate) {
+                let path = this.$route.path + '?cate=' + cate;
+                this.$router.push(path);
+                this.$router.go(0)
             }
         }
     }
@@ -60,16 +61,34 @@
         left: 1px;
     }
 
-    .sider-cate-text {
-        margin-top: 20px;
+    .sider-cate-div {
+        margin-top: 30px;
+    }
+
+    .collapse-item-cate {
+        margin-left: 50px;
+    }
+
+    .cate-name {
+        margin-bottom: 15px;
+        margin-left: 20px;
     }
 
     .post-nav-a {
         color: #808080;
         text-decoration: none;
     }
+
     .post-nav-a:hover {
         color: black;
         text-decoration: underline;
+    }
+
+    .cate-title {
+        margin-bottom: 0px;
+    }
+
+    .divider { 
+        margin-top: 15px;
     }
 </style>
--- front-end/src/views/admin/Admin.vue
+++ front-end/src/views/admin/Admin.vue
@@ -1,15 +1,35 @@
     
 <template>
   <div>
-    <h1 v-show="clickOneManger" class="title">后台管理页面</h1>
-    <div v-show="clickOneManger" class="col">
+    <!-- <h1 v-show="clickOneManger" class="title">后台管理页面</h1> -->
+    <div v-show="clickOneManger">
     <b-container class="bv-example-row">
-      <b-row>
-        <b-col @click="clickManagePost"><router-link :to="{name: 'new-post'}"> 写文章 </router-link> </b-col>
-        <b-col ><router-link :to="{name: 'post-list'}"> 文章列表 </router-link> </b-col>
-        <b-col ><router-link :to="{name: 'admin-category'}">文章类别管理</router-link></b-col>
+      <!-- <b-row>
+        <b-col > </b-col>
+        <b-col > </b-col>
+        <b-col ></b-col>
         <b-col >个人信息管理</b-col>
-      </b-row>
+      </b-row> -->
+      <el-row class="tac">
+        <el-col :span="3">
+          <h5>后台管理页面</h5>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose">
+            <el-menu-item index="1"> <!-- 创建文章的时候， 隐藏导航-->
+              <div @click="clickManagePost();toSubAdmin('new-post')">写文章</div>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <div slot="title" @click="toSubAdmin('post-list')">文章列表</div>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <div slot="title" @click="toSubAdmin('admin-category')">文章类别管理</div>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
     </b-container>
     </div>
     <router-view></router-view>
@@ -26,8 +46,18 @@ export default {
   methods: {
       clickManagePost(){
           this.clickOneManger = false;
+      },
+      toSubAdmin(subAdmin) {
+        this.$router.push({'name': subAdmin});
+      },
+      popstate(){ // 回退的时候 进入这个页面，展示出来侧边导航
+        this.clickOneManger = true;
       }
   },
+  mounted(){
+    let nowPath = this.$route.path;
+    window.addEventListener('popstate', this.popstate, false);
+  }
 };
 </script>
 
@@ -38,6 +68,9 @@ export default {
   margin-top: 20px;
 }
 
+.bv-example-row{
+  margin-top: 20px;
+}
 .col {
     width: 1000px;
     margin: 0 auto;
